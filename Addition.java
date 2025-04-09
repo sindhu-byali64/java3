9f7cec3dee6a5d1561c994528b91af3f3f3b3447
sindhu-byali64 <sindhutbyali6@gamil.com>
2025-04-09 15:11:25 +0530

    save

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package java2;
+
+public class Addition {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int sum= a + b;
+    System.out.println("sum of a & b is: " + sum);
+}
+}
