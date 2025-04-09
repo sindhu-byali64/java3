d74e169e8be2ce3f6f578674298771378711eed7
sindhu-byali64 <sindhutbyali6@gamil.com>
2025-04-09 15:16:39 +0530

    save

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+package java2;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sub= a - b;
+        System.out.println("sub of a & b is: " + sub);
+    }
+}
